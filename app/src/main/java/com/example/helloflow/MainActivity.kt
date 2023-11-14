4afc2558df7b13dd83faf02c82783c6cb33295df
刘金华 <1533761530@qq.com>
2023-11-14 09:29:57 +0800

    hello kotlin flow

--- app/src/main/java/com/example/helloflow/MainActivity.kt
+++ app/src/main/java/com/example/helloflow/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.activity.viewModels
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
@@ -30,12 +31,33 @@ class MainActivity : AppCompatActivity() {
 //                mainViewModel.timeFlow.collect{
 //
 //                }
-                //下面是正确写法，开启子协程单独调用collect
+
+                /*
+                    下面是正确写法，开启子协程单独调用collect。
+                    调用collect函数就相当于把水龙头接到水管上并打开，这样从水源发送过来的任何数据，
+                    我们在水龙头这边都可以接收到，然后再把接收到的数据更新到TextView上面即可。
+                 */
                 launch {
-                    mainViewModel.timeFlow.collect{time ->
-                        textView.text = time.toString()
+//                    mainViewModel.timeFlow.collect{time ->
+//                        textView.text = time.toString() //依次显示1，2，3，4...
+                        /*
+                            模拟流速不均问题，在水源处我们是每秒种发送一条数据，结果在水龙头这里要3秒钟才能处理一条数据。
+                            水龙头处理数据速度过慢，导致管道中存在大量的积压数据，并且积压的数据会一个个继续传递给水龙头，即使这些数据已经过期了。
+
+                         */
+//                        delay(3000)
+//                    }
+                    /*
+                        解决思路：只要有更新的数据过来，如果上次的数据还没有处理完，那么我们就直接把它取消掉，立刻去处理最新的数据即可。
+                            在Flow当中实现这样的功能，只需要借助collectLatest函数就能做到，
+                            collectLatest函数只接收处理最新的数据。
+                            如果有新数据到来了而前一个数据还没有处理完，则会将前一个数据剩余的处理逻辑全部取消。
+                     */
+                    mainViewModel.timeFlow.collectLatest {
+                        textView.text = it.toString()
                         delay(3000)
                     }
+
                 }
                 launch {
 //                    mainViewModel.timeFlow2.collect{time ->
--- app/src/main/java/com/example/helloflow/MainViewModel.kt
+++ app/src/main/java/com/example/helloflow/MainViewModel.kt
@@ -8,14 +8,24 @@ import kotlinx.coroutines.flow.flow
  *
  * @Author： LJH
  * @Time： 2023/11/13
- * @description：
+ * @description：响应式编程就像是使用水龙头来接水一样。
+ * 那么整个过程中最重要的部分一共有3处：水源、水管和水龙头。
+ *  1.水源也就是我们的数据源，这部分是需要我们自己处理的。
+ *  2.水龙头是最终的接收端，可能是要展示给用户的，这部分也需要我们自己处理。
+ *  3.水管则是实现响应式编程的基建部分，这部分是由Flow封装好提供给我们的，并不需要我们自己去实现。
  */
 class MainViewModel:ViewModel() {
 
+
+    /*
+        使用flow构建函数构建出的Flow是属于Cold Flow，也叫做冷流。
+        所谓冷流就是在没有任何接受端的情况下，Flow是不会工作的。
+        只有在有接受端（水龙头打开）的情况下，Flow函数体中的代码就会自动开始执行。
+     */
     val timeFlow = flow<Int> {
-        var time = 0
+        var time = 0 //水源
         while (true) {
-            emit(time)
+            emit(time) //数据发送器，把传入的参数发送到水管中
             delay(1000)
             time++
         }
